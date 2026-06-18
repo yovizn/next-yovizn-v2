3b7925ee95ae1b720fdfbd042166ab39fa2b0dab
yovizn <yovizulkarnaen@gmail.com>
2026-06-18 13:34:35 +0700

    refactor(homepage): use useParallax in OverviewImage (replace bespoke useScroll block)

--- src/module/homepage/views/overview-image.view.tsx
+++ src/module/homepage/views/overview-image.view.tsx
@@ -1,22 +1,29 @@
 'use client'
 
 import Image from 'next/image'
+import { motion } from 'motion/react'
+
+import { useParallax } from '@/components/animations/scroll'
 
 import WhiteOne from '@public/images/white-one.jpg'
-import { motion, useScroll, useTransform } from 'motion/react'
-import { useMatchMedia } from '@/hooks/useMedia.hook'
 
 export function OverviewImage() {
-  const { scrollYProgress } = useScroll({
+  const { ref, value, enabled } = useParallax({
     offset: ['start start', 'end start'],
+    range: ['-50px', '50px'],
+    axis: 'y',
+    disabledOnMobile: true,
   })
 
-  const isDesktop = useMatchMedia(640, 'min')
-  const y = useTransform(scrollYProgress, [0, 1], ['-50px', '50px'])
-
   return (
-    <div className="bg-background col-span-4 aspect-video h-auto overflow-clip lg:row-span-2 lg:aspect-auto">
-      <motion.div style={isDesktop ? { y } : {}} className="relative h-[calc(100%+100px)] w-full">
+    <div
+      ref={ref as React.Ref<HTMLDivElement>}
+      className="bg-background col-span-4 aspect-video h-auto overflow-clip lg:row-span-2 lg:aspect-auto"
+    >
+      <motion.div
+        style={enabled ? { y: value } : undefined}
+        className="relative h-[calc(100%+100px)] w-full"
+      >
         <Image
           src={WhiteOne}
           alt="Image White One"
